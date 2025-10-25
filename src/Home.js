be95de042c7d299f4df490e564d66053c44cbb92
Nitesh Kumar <82534120+Nitesh8877@users.noreply.github.com>
2025-10-25 15:57:41 +0530

    Refactor Home component with new layout and buttons
    Updated the Home component layout and styling.

--- src/Home.js
+++ src/Home.js
@@ -1,17 +1,31 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
-import {Button} from 'reactstrap'
+import { Link } from 'react-router-dom'
 import './App.css'
+
 export default function Home() {
   return (
-    <>
     <div className='home'>
-        <Button ><Link to="/generateCertificate">Generate Certificate</Link> </Button>
-        <Button ><Link to="/verifyCeritficate">Verify Certificate</Link></Button>
-        </div>
-    <div>
-    <img src='https://d2vyhi5ouo1we3.cloudfront.net/force_jpg/aHR0cHM6Ly9pbWFnZXMuYmFubmVyYmVhci5jb20vcmVxdWVzdHMvaW1hZ2VzLzAwOC85MjQvNTc5L29yaWdpbmFsL2VhYzQyY2FiZjM5YzIxY2Y4NThlNWY4NDRlZmM0YTA1MjJmOGUxNzkucG5nPzE2MzI4MDgzMDI=/image.jpg'/>
+      <div className="home-header">
+        <h1>Certificate Generator</h1>
+        <p>Create professional certificates with multiple styles and verify them securely</p>
+      </div>
+      
+      <div className="button-container">
+        <button className="btn-primary">
+          <Link to="/generateCertificate">Generate Certificate</Link>
+        </button>
+        <button className="btn-secondary">
+          <Link to="/verifyCertificate">Verify Certificate</Link>
+        </button>
+      </div>
+
+      <div className="home-image-container">
+        <img 
+          src='https://d2vyhi5ouo1we3.cloudfront.net/force_jpg/aHR0cHM6Ly9pbWFnZXMuYmFubmVyYmVhci5jb20vcmVxdWVzdHMvaW1hZ2VzLzAwOC85MjQvNTc5L29yaWdpbmFsL2VhYzQyY2FiZjM5YzIxY2Y4NThlNWY4NDRlZmM0YTA1MjJmOGUxNzkucG5nPzE2MzI4MDgzMDI=/image.jpg' 
+          alt="Certificate Example" 
+          className="home-image"
+        />
+      </div>
     </div>
-    </>
   )
 }
